be39c780d28150dd88af816884a4f5c8e5a03194
Jesus Ramos <125627643+JesusRms1@users.noreply.github.com>
2023-11-13 20:11:55 -0800

    Update PArt A app.js

--- project9/app.js
+++ project9/app.js
@@ -8,12 +8,15 @@ var camera = new OrbitCamera(appInput);
 
 var sphereGeometry = null; // this will be created after loading from a file
 var groundGeometry = null;
+var barrelGeometry = null;
+var lightGeometry = null;
 
 var projectionMatrix = new Matrix4();
-var lightDirection = new Vector3(5, 3, 0);
+var lightPosition = new Vector4(4, 1.5, 0, 0);
 
 // the shader that will be used by each piece of geometry (they could each use their own shader but in this case it will be the same)
 var phongShaderProgram;
+var flatColorShaderProgram;
 
 // auto start the app when the html page is ready
 window.onload = window['initializeAndStartRendering'];
@@ -23,7 +26,11 @@ var loadedAssets = {
     phongTextVS: null, phongTextFS: null,
     sphereJSON: null,
     marbleImage: null,
-    crackedMudImage: null
+    crackedMudImage: null,
+    barrelImage: null,
+    barrelJSON: null,
+    flatColorVS: null,
+    flatColorFS: null
 };
 
 // -------------------------------------------------------------------------
@@ -58,10 +65,14 @@ function initGL(canvas) {
 function loadAssets(onLoadedCB) {
     var filePromises = [
         fetch('./shaders/phong.vs.glsl').then((response) => { return response.text(); }),
-        fetch('./shaders/phong.directionlit.fs.glsl').then((response) => { return response.text(); }),
+        fetch('./shaders/phong.pointlit.fs.glsl').then((response) => { return response.text(); }),
         fetch('./data/sphere.json').then((response) => { return response.json(); }),
         loadImage('./data/marble.jpg'),
-        loadImage('./data/crackedMud.png')
+        loadImage('./data/crackedMud.png'),
+        fetch('./data/barrel.json').then((response) => { return response.json(); }),
+        loadImage('./data/barrel.png'),
+        fetch('./shaders/flat.color.vs.glsl').then((response) => { return response.text(); }),
+        fetch('./shaders/flat.color.fs.glsl').then((response) => { return response.text(); })
     ];
 
     Promise.all(filePromises).then(function(values) {
@@ -71,6 +82,10 @@ function loadAssets(onLoadedCB) {
         loadedAssets.sphereJSON = values[2];
         loadedAssets.marbleImage = values[3];
         loadedAssets.crackedMudImage = values[4];
+        loadedAssets.barrelJSON = values[5];
+        loadedAssets.barrelImage = values[6];
+        loadedAssets.flatColorVS = values[7];
+        loadedAssets.flatColorFS = values[8];
     }).catch(function(error) {
         console.error(error.message);
     }).finally(function() {
@@ -92,9 +107,26 @@ function createShaders(loadedAssets) {
         worldMatrixUniform: gl.getUniformLocation(phongShaderProgram, "uWorldMatrix"),
         viewMatrixUniform: gl.getUniformLocation(phongShaderProgram, "uViewMatrix"),
         projectionMatrixUniform: gl.getUniformLocation(phongShaderProgram, "uProjectionMatrix"),
-        lightDirectionUniform: gl.getUniformLocation(phongShaderProgram, "uLightDirection"),
+        lightPositionUniform: gl.getUniformLocation(phongShaderProgram, "uLightPosition"),
         cameraPositionUniform: gl.getUniformLocation(phongShaderProgram, "uCameraPosition"),
         textureUniform: gl.getUniformLocation(phongShaderProgram, "uTexture"),
+    };
+
+    flatColorShaderProgram = createCompiledAndLinkedShaderProgram(loadedAssets.flatColorVS, loadedAssets.flatColorFS);
+
+    flatColorShaderProgram.attributes = {
+        vertexPositionAttribute: gl.getAttribLocation(flatColorShaderProgram, "aVertexPosition"),
+        vertexNormalsAttribute: gl.getAttribLocation(flatColorShaderProgram, "aNormal"),
+        vertexTexcoordsAttribute: gl.getAttribLocation(flatColorShaderProgram, "aTexcoords")
+    };
+
+    flatColorShaderProgram.uniforms = {
+        worldMatrixUniform: gl.getUniformLocation(flatColorShaderProgram, "uWorldMatrix"),
+        viewMatrixUniform: gl.getUniformLocation(flatColorShaderProgram, "uViewMatrix"),
+        projectionMatrixUniform: gl.getUniformLocation( flatColorShaderProgram, "uProjectionMatrix"),
+        lightPositionUniform: gl.getUniformLocation( flatColorShaderProgram, "uLightPosition"),
+        cameraPositionUniform: gl.getUniformLocation(flatColorShaderProgram, "uCameraPosition"),
+        textureUniform: gl.getUniformLocation(flatColorShaderProgram, "uTexture"),
     };
 }
 
@@ -114,6 +146,13 @@ function createScene() {
     sphereGeometry = new WebGLGeometryJSON(gl, phongShaderProgram);
     sphereGeometry.create(loadedAssets.sphereJSON, loadedAssets.marbleImage);
 
+    barrelGeometry = new WebGLGeometryJSON(gl, phongShaderProgram);
+    barrelGeometry.create(loadedAssets.barrelJSON, loadedAssets.barrelImage);
+
+    lightGeometry = new WebGLGeometryJSON(gl, flatColorShaderProgram);
+    lightGeometry.create(loadedAssets.sphereJSON, loadedAssets.marbleImage);
+
+
     // Scaled it down so that the diameter is 3
     var scale = new Matrix4().makeScale(0.03, 0.03, 0.03);
 
@@ -123,6 +162,14 @@ function createScene() {
     sphereGeometry.worldMatrix.makeIdentity();
     sphereGeometry.worldMatrix.multiply(translation).multiply(scale);
 
+    var scale2 = new Matrix4().makeScale(0.3, 0.3, 0.3);
+    var translation2 = new Matrix4().makeTranslation(-5, 2, -5);
+
+    barrelGeometry.worldMatrix.makeIdentity().multiply(translation2).multiply(scale2);
+
+    var scale3 = new Matrix4().makeScale(0.006, 0.006, 0.006);
+    var translation3 = new Matrix4().makeTranslation(4, 1.5, 0, 0);
+    lightGeometry.worldMatrix.makeIdentity().multiply(translation3).multiply(scale3);
 }
 
 // -------------------------------------------------------------------------
@@ -130,34 +177,45 @@ function updateAndRender() {
     requestAnimationFrame(updateAndRender);
 
     var aspectRatio = gl.canvasWidth / gl.canvasHeight;
-    var rotationMatrix = new Matrix4();
+    var rotationMatrix = new Matrix4().makeRotationY(45.0 * time.deltaTime);
+
+    // rotates light position around earth
+    lightPosition = rotationMatrix.clone().multiplyVector(lightPosition);
+
+    // trying to rotate sphere around earth like light
+    // lightGeometry.worldMatrix.makeIdentity();
+
+    var translationForLight = new Matrix4().makeTranslation(lightPosition);
+    // moonMatrix.multiply(earthTransform).multiply(rotation).multiply(translationForMoon);
+
+    lightGeometry.worldMatrix.multiply(rotationMatrix);
+    // lightGeometry.worldMatrix.multiply(translationForLight);
 
     // todo #10
     // add keyboard controls for changing light direction here
-    if (appInput.up) {
-        rotationMatrix.makeIdentity();
-        rotationMatrix.makeRotationX(-2);
-        lightDirection = rotationMatrix.multiplyVector(new Vector4(lightDirection.x, lightDirection.y, lightDirection.z, 1));
-    }
+    // if (appInput.up) {
+    //     rotationMatrix.makeIdentity();
+    //     rotationMatrix.makeRotationX(-2);
+    //     lightPosition = rotationMatrix.multiplyVector(new Vector4(lightPosition.x, lightPosition.y, lightPosition.z, 1));
+    // }
 
-    if (appInput.down) {
-        rotationMatrix.makeIdentity();
-        rotationMatrix.makeRotationX(2);
-        lightDirection = rotationMatrix.multiplyVector(new Vector4(lightDirection.x, lightDirection.y, lightDirection.z, 1));
-    }
+    // if (appInput.down) {
+    //     rotationMatrix.makeIdentity();
+    //     rotationMatrix.makeRotationX(2);
+    //     lightPosition = rotationMatrix.multiplyVector(new Vector4(lightPosition.x, lightPosition.y, lightPosition.z, 1));
+    // }
 
-    if (appInput.left) {
-        rotationMatrix.makeIdentity();
-        rotationMatrix.makeRotationZ(2);
-        lightDirection = rotationMatrix.multiplyVector(new Vector4(lightDirection.x, lightDirection.y, lightDirection.z, 1));
-    }
+    // if (appInput.left) {
+    //     rotationMatrix.makeIdentity();
+    //     rotationMatrix.makeRotationZ(2);
+    //     lightPosition = rotationMatrix.multiplyVector(new Vector4(lightPosition.x, lightPosition.y, lightPosition.z, 1));
+    // }
 
-    if (appInput.right) {
-        rotationMatrix.makeIdentity();
-        rotationMatrix.makeRotationZ(-2);
-        lightDirection = rotationMatrix.multiplyVector(new Vector4(lightDirection.x, lightDirection.y, lightDirection.z, 1));
-    }
-
+    // if (appInput.right) {
+    //     rotationMatrix.makeIdentity();
+    //     rotationMatrix.makeRotationZ(-2);
+    //     lightPosition = rotationMatrix.multiplyVector(new Vector4(lightPosition.x, lightPosition.y, lightPosition.z, 1));
+    // }
 
     time.update();
     camera.update(time.deltaTime);
@@ -172,12 +230,14 @@ function updateAndRender() {
     gl.useProgram(phongShaderProgram);
     var uniforms = phongShaderProgram.uniforms;
     var cameraPosition = camera.getPosition();
-    gl.uniform3f(uniforms.lightDirectionUniform, lightDirection.x, lightDirection.y, lightDirection.z);
+    gl.uniform3f(uniforms.lightPositionUniform, lightPosition.x, lightPosition.y, lightPosition.z);
     gl.uniform3f(uniforms.cameraPositionUniform, cameraPosition.x, cameraPosition.y, cameraPosition.z);
 
     projectionMatrix.makePerspective(45, aspectRatio, 0.1, 1000);
     groundGeometry.render(camera, projectionMatrix, phongShaderProgram);
     sphereGeometry.render(camera, projectionMatrix, phongShaderProgram);
+    barrelGeometry.render(camera, projectionMatrix, phongShaderProgram);
+    lightGeometry.render(camera, projectionMatrix, flatColorShaderProgram);
 }
 
 // EOF 00100001-10
